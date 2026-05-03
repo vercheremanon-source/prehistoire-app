de12032a5a106ec1a542e99d06359784c613b6fe
vercheremanon-source <verchere.manon@gmail.com>
2026-05-03 23:00:54 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const DATA = {
+  events: [
+    {
+      year: -300000,
+      title: "Homo sapiens",
+      description: "Apparition en Afrique, développement culturel."
+    },
+    {
+      year: -40000,
+      title: "Disparition Néandertal",
+      description: "Climat, compétition avec sapiens, hybridation."
+    }
+  ],
+
+  sites: [
+    {
+      name: "Lascaux",
+      coords: [45.053, 1.166],
+      description: "~17 000 ans, peintures rupestres."
+    },
+    {
+      name: "Chauvet",
+      coords: [44.396, 4.416],
+      description: "~36 000 ans, art très ancien."
+    }
+  ]
+};
